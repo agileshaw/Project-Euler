53300a34cbf92ca38b336ac342d316e25067d336
agileshaw <agileshaw@gmail.com>
2019-05-19 22:04:50 -0400

    Problem0012 Perl Solution

---
+++ Perl/Problem0012.pl
@@ -0,0 +1,35 @@
+# Problem0012 - Highly divisible triangular number
+#
+# https://github.com/agileshaw/Project-Euler
+
+my $count = 0;
+my $natural = 1;
+my $result = 0;
+sub numOfFactors;
+
+while ($count < 500) {
+    $result += $natural;
+    $natural++;
+    $count = numOfFactors($result);
+}
+
+print "The highly divisible triangular number is: $result\n";
+
+sub numOfFactors
+{
+    my $num = shift;
+    my $root = int(sqrt($num));
+    my $count = 0;
+
+    for my $i (1..$root) {
+        if ($num % $i == 0) {
+            $count += 2;
+        }
+    }
+
+    if ($root * $root == $num) {
+        $count--;
+    }
+
+    return $count;
+}
