7def8b8561fd188a3fc5d828d4cf0adc4b256dfb
agileshaw <agileshaw@gmail.com>
2019-05-19 22:04:24 -0400

    minor syntax fix

--- Perl/Problem0007.pl
+++ Perl/Problem0007.pl
@@ -2,23 +2,23 @@
 #
 # https://github.com/agileshaw/Project-Euler
 
-my $num = 2;
+my $num = 1;
 my $count = 0;
 my $check;
 
 while ($count < 10001) {
+    $num += 1;
     $check = 1;
-    foreach my $i (2..int(sqrt($num))) {
+
+    for my $i (2..int(sqrt($num))) {
         if ($num % $i == 0) {
             $check = 0;
         }
     }
+    
     if ($check == 1) {
         $count++;
     }
-    if ($count == 10001) {
-        print "The 10001st prime is: $num\n";
-    }
-    $num += 1;
 }
-    
+
+print "The 10001st prime is: $num\n";
