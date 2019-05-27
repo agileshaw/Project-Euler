cd4e40bb5757170bd0bab63825324ad4f3db3e05
agileshaw <agileshaw@gmail.com>
2019-05-26 22:37:41 -0400

    Problem0014 C Solution

---
+++ C/Problem0014.c
@@ -0,0 +1,39 @@
+/**
+ * Problem0014 - Longest Collatz sequence
+ * 
+ * https://github.com/agileshaw/Project-Euler
+ **/
+
+#include <stdio.h>
+
+unsigned long long collatz(unsigned long long num)
+{
+    unsigned long long count = 0;
+
+    while (num != 1) {
+        if (num % 2 == 0)
+            num = num / 2;
+        else
+            num = 3 * num + 1;
+        count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+    unsigned long long i, count, max = 0, result;
+
+    for (i = 2; i < 1000000; i++) {
+        count = collatz(i);
+        if (count > max) {
+            max = count;
+            result = i;
+        }
+    }
+
+    printf("The number which generates the longest collatz sequence is: %llu\n", result);
+
+    return 0;
+}
