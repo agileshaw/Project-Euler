ed9a745689ea5465319cdc1209af86c6251a9f4f
agileshaw <agileshaw@gmail.com>
2019-06-10 10:03:03 -0400

    Problem0067 Python Solution

--- Python/Problem0067.py
+++ Python/Problem0067.py
@@ -1,9 +1,13 @@
-# Problem0018 - Maximum path sum I
+# Problem0067 - Maximum path sum II
 #
 # https://github.com/agileshaw/Project-Euler
 
 if __name__ == "__main__":
-    
+    num = []
+    with open('p067_triangle.txt') as f:
+        for line in f:
+            num.append([int(x) for x in line.split()])
+
     for i in range(len(num)-2, -1, -1):
         for j in range(i+1):
             num[i][j] += max(num[i+1][j], num[i+1][j+1])
