f8f56ae80c5a7e8b9299bef06de3ac8eac4d36bb
agileshaw <agileshaw@gmail.com>
2019-06-16 13:29:25 -0400

    Problem0020 C Solution

---
+++ C/Problem0020.c
@@ -0,0 +1,43 @@
+/**
+ * Problem0016 - Power digit sum
+ * 
+ * https://github.com/agileshaw/Project-Euler
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bigIntMultiply(int *num, int size, int multiplier)
+{
+    int i, tmp, carry = 0;
+
+    for (i = 0; i < size; i++)
+    {
+        tmp = multiplier * num[i] + carry;
+        num[i] = tmp % 10;
+        carry = tmp / 10;
+    }
+}
+
+int main()
+{
+    int i, size = 200, result = 0;
+    int *num = calloc(size, sizeof(int));
+
+    if (num == NULL) {
+        printf("Error allocating memory\n");
+        exit(1);
+    }
+
+    num[0] = 1;
+    for (i = 1; i <= 100; i++)
+        bigIntMultiply(num, size, i);
+
+    for (i = 0; i < size; i++)
+        result += num[i];
+
+    printf("The sum of the digits is: %d\n", result);
+    free(num);
+
+    return 0;
+}
