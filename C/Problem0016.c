c6305618f75436c9bb2c8d07cafb550e612a2f7b
agileshaw <agileshaw@gmail.com>
2019-06-16 13:29:09 -0400

    Problem0016 C Solution

--- C/Problem0016.c
+++ C/Problem0016.c
@@ -5,21 +5,37 @@
  **/
 
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+
+void bigIntMultiply (int* num, int size, int multiplier) {
+    int i, tmp, carry = 0;
+    
+    for (i = 0; i < size; i++) {
+        tmp = multiplier * num[i] + carry;
+        num[i] = tmp % 10;
+        carry = tmp / 10;
+    }
+}
 
 int main ()
 {
-    unsigned long long num, result = 0;
-    int digit;
-
-    num = (unsigned long long)(pow(2, 1000));
-    printf("%llu\n", num);
-    while (num > 0) {
-        digit = num % 10;
-        result += digit;
-        num /= 10;
+    int i, size = 400, result = 0;
+    int *num = calloc(size, sizeof(int));
+    
+    if (num == NULL) {
+        printf("Error allocating memory\n");
+        exit(1);
     }
-    printf("The sum of the digits is: %llu\n", result);
+
+    num[0] = 1;
+    for (i = 0; i < 1000; i++)
+        bigIntMultiply(num, size, 2);
+
+    for (i = 0; i < size; i++) 
+        result += num[i];
+    
+    printf("The sum of the digits is: %d\n", result);
+    free(num);
 
     return 0;
 }
