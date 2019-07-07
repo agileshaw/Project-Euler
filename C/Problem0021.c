8fe8e08a12b4993d9aab3effe2e127e79b4f8c85
agileshaw <agileshaw@gmail.com>
2019-07-06 23:17:18 -0400

    Problem0020 C Solution (Incomplete)

---
+++ C/Problem0021.c
@@ -0,0 +1,49 @@
+/**
+ * Problem0021 - Amicable numbers
+ * 
+ * https://github.com/agileshaw/Project-Euler
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int divisorSum(int num)
+{
+	int sum = 1, i;
+	
+	for (i = 2; i <= sqrt(num+1); i++) {
+		if (num % i == 0){
+			if (num / i == i)
+				sum += i;
+			else
+				sum += i + num/i;
+		}
+	}
+
+	return sum;
+}
+
+int main()
+{
+	int i, size = 10000, result = 0;
+	int* num = calloc(size, sizeof(int));
+
+	if (num == NULL) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+	
+	for (i = 2; i < size; i++)
+		num[i] = divisorSum(i);
+
+	for (i = 1; i < size; i++) {
+		if (num[num[i]] == i && num[i] != i)
+			result += i;
+	}
+
+	printf("The sum of amicable numbers is: %d\n", result);
+	free(num);
+
+	return 0;
+}
